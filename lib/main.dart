d1b595ac4f71170ed6b5a79ae4e120b0ef644f21
shreyassai123 <shreyassai2@gmail.com>
2021-11-27 17:41:34 +0530

    Removed need for logging in everytime

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mobile_app/screens/landing.dart';
 import 'package:mobile_app/screens/login.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
+  print(FirebaseAuth.instance.currentUser);
 }
 
 class MyApp extends StatelessWidget {
@@ -24,10 +30,18 @@ class MyApp extends StatelessWidget {
     };
 
     MaterialColor primaryColor = MaterialColor(0xff1c817c, color);
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: primaryColor),
-      home: const LoginPage(),
-    );
+    if (FirebaseAuth.instance.currentUser == null) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: primaryColor),
+        home: const LoginPage(),
+      );
+    } else {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: primaryColor),
+        home: LandingPage(),
+      );
+    }
   }
 }
